879d0903d2a5b9f436f8a8ed60f94bc7265e2254
Leonardo Ferreira <leonardoff.eng@gmail.com>
2025-11-19 20:54:38 +0100

    fix favicon

--- app/root.tsx
+++ app/root.tsx
@@ -11,7 +11,7 @@ import type { Route } from "./+types/root";
 import "./app.css";
 
 export const links: Route.LinksFunction = () => [
-  { rel: "icon", href: "/favicon.png", type: "image/png" },
+  { rel: "icon", href: "/burger/favicon.png", type: "image/png" },
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
   {
     rel: "preconnect",
